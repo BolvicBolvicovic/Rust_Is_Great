24492a57d9604ca97dc7f509bade1c1495dbb775
Victor Cornille <vcornill@c1r3p4.42lehavre.fr>
2024-02-14 19:18:32 +0100

    update

--- CrabShell/src/main.rs
+++ CrabShell/src/main.rs
@@ -1,19 +1,3 @@
-use nom::{IResult, Needed, error::Error, }
-
-
-struct CommandLine {
-
-}
-
-enum ErrParsing {
-	
-}
-
-fn parse(line: String) -> IResult<String, CommandLine, ErrParsing> {
-
-
-}
-
 fn main() {
 
 }
