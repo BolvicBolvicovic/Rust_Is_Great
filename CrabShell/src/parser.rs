4799061a0af02a17369333e97fe7ceae086012b3
Victor Cornille <vcornill@c1r3p4.42lehavre.fr>
2024-02-15 20:35:48 +0100

    update

--- CrabShell/src/parser.rs
+++ CrabShell/src/parser.rs
@@ -1,6 +1,10 @@
+mod enum;
 use nom::IResult;
 use nom::number::complete::be_u16;
 use nom::bytes::complete::take;
+
+
+/// Based on POSIX specification https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/utilities/V3_chap02.html
 
 pub fn length_value(input: &[u8]) -> IResult<&[u8],&[u8]> {
     let (input, length) = be_u16(input)?;
