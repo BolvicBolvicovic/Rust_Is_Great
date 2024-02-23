7b6ea1645dcc6c63578551105c96a1d7f00a754a
Bolvic <victor.bolheme@gmail.com>
2024-02-23 19:44:07 +0100

    end of the road

---
+++ wasm-game-of-life/pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+*/
+  tick(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+* @returns {number}
+*/
+  get_width(): number;
+/**
+* @returns {number}
+*/
+  get_height(): number;
+/**
+* @returns {number}
+*/
+  get_cells(): number;
+/**
+* @returns {string}
+*/
+  render(): string;
+}
