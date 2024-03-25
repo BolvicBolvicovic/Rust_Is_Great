0519264d9481af81c1450927b3bb6b028d785127
Bolvic <victor.bolheme@gmail.com>
2024-03-25 15:49:47 +0100

    grep done

---
+++ elevation/grep_tool/src/main.rs
@@ -0,0 +1,45 @@
+use std::{
+	env,
+	fs::File,
+	io::{self, BufRead}
+};
+
+struct	FullFile{
+	file:	File,
+	name:	String,
+}
+
+fn get_files(mut input: env::Args) -> Vec<FullFile> {
+	let mut files = Vec::<FullFile>::new();
+	for path in input.by_ref() {
+		let file = File::open(path.clone());
+		files.push(FullFile {
+			file: file.expect("Panic: Path file incorrect."),
+			name: path,
+		});
+	}
+	files
+}
+
+fn read_pattern(mut input: env::Args) -> (String, env::Args) {
+	input.next(); //skipping the executable
+	(input.next().expect("Panic: No argument provided."), input)
+}
+
+fn find_pattern(pattern: String, files: Vec<FullFile>) {
+	for file in files.iter() {
+		let mut lines = io::BufReader::new(file.file.try_clone().unwrap()).lines();
+		for line in lines.by_ref().enumerate() {
+			if let Some(_) = line.1.as_ref().expect("Panic: File cannot be read.").find(&pattern) {
+				println!("{} at line {}: {}", file.name, line.0 + 1, line.1.unwrap());
+			}
+		}
+	}
+}
+
+fn main() {
+	let input = std::env::args();
+	let (pattern, input) = read_pattern(input);
+	let files = get_files(input);
+	find_pattern(pattern, files);
+}
