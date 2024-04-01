483e85c2613f312fa6bf80c499942d10ec1464aa
Bolvic <victor.bolheme@gmail.com>
2024-04-02 00:33:48 +0200

    everything works: can add URLs and delete them from database

--- elevation/url-shortener/src/main.rs
+++ elevation/url-shortener/src/main.rs
@@ -1,15 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-
 mod url;
 
 use rocket_dyn_templates::{ context, Template };
 use rocket_sync_db_pools::{ database, rusqlite::Connection };
 use url::{ Url, GetUrl };
-use std::collections::HashMap;
+use std::{ 
+    ops::DerefMut,
+    collections::HashMap,
+    path::PathBuf,
+};
 use rocket::form::Form;
-use std::ops::DerefMut;
 
 #[database("sqlite_db")]
 struct	DatabaseConnection(Connection);
@@ -28,13 +30,21 @@ fn insert_url(conn: &Connection, url: Url) {
 fn to_shorten_url(node: &mut GetUrl) -> Url {
     let start = match node.url.find("://") {
         Some(start) => start + 3,
-        None => 0,
+        None => { 
+            let mut new_url = "https://".to_string();
+            new_url.push_str(node.url.as_str()); 
+            node.url = new_url;
+            8
+            },
     };
 	let end	= match node.url.rfind(".com") {
         Some(end)   => end + 3,
         None        => match node.url.rfind(".fr") {
             Some(end)   => end + 2,
-            None        => node.url.len() - 1,
+            None        => {
+                node.url.push_str(".com");
+                node.url.rfind(".com").unwrap() + 3
+            },
         }
     };
 	Url::new(node.url.clone(), String::from(&node.url[start..=end]))
@@ -56,20 +66,27 @@ fn render(conn: &Connection) -> Template {
     Template::render("index", context!{ urls: urls_vec })
 }
 
+fn delete_data(conn: &Connection, url: String) {
+    conn.execute("DELETE FROM urls WHERE shorten_url = (?1)", [&url])
+        .expect("Panic: Could not delete data from database");
+}
+
 #[get("/")]
 async fn index(conn: DatabaseConnection) -> Template {
     conn.run(|c| render(c)).await
 }
 
-#[post("/", data = "<form>")]
+#[post("/submit", data = "<form>")]
 async fn submit(conn: DatabaseConnection, mut form: Form<GetUrl>) -> Template {
 	let url		= to_shorten_url(form.deref_mut());
 	conn.run(|c| insert_url(c, url)).await;
     conn.run(|c| render(c)).await
 }
 
-async fn delete(conn: DatabaseConnection) -> Template {
-    
+#[delete("/delete/<name>")]
+async fn delete(conn: DatabaseConnection, name: PathBuf) -> Template {
+    let string_url = name.display().to_string();
+    conn.run(move |c| delete_data(c, string_url)).await;
     conn.run(|c| render(c)).await
 }
 
@@ -87,5 +104,5 @@ fn rocket() -> _ {
 	rocket::build()
         .attach(Template::fairing())
 		.attach(DatabaseConnection::fairing())
-		.mount("/", routes![index, submit])
+		.mount("/", routes![index, submit, delete])
 }
