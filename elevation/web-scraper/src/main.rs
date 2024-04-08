c0cd1583cf4f9c7e5b04827ec5a818828611c738
Bolvic <victor.bolheme@gmail.com>
2024-04-08 12:57:17 +0200

    problem with imgs i try to dl and fetching a relative url without a base

--- elevation/web-scraper/src/main.rs
+++ elevation/web-scraper/src/main.rs
@@ -1,10 +1,10 @@
 use futures::future::join_all;
 use reqwest::Client;
 use indicatif::ProgressBar;
-use std::{error::Error, path, time::Duration};
+use std::{fs::File, io::Write, path::Path, time::Duration};
 use colored::Colorize;
-use clap::{arg, command, Arg, ArgAction, Error};
-
+use clap::{command, Arg, ArgAction};
+use scraper::{Html, Selector};
 
 struct Query {
     r_flag: bool,
@@ -24,49 +24,62 @@ impl Query {
     }
 }
 
+
+async fn download(image: String, img_num: usize, path: String, client: Client) -> Result<(), Box<dyn std::error::Error>> {
+    let response = async move {
+        let future = client.get(image).send().await?;
+        future.text().await
+    }.await?;
+    println!("{}", response);
+    let name = format!("{}image{}.png", path, img_num); 
+    let filename = Path::new(name.as_str())
+        .file_name()
+        .and_then(|s| s.to_str())
+        .unwrap();
+
+    let mut file = File::create(filename)?;
+    file.write_all(response.as_bytes())?;
+    Ok(())
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
     
     let matches = command!()
+        .arg_required_else_help(true)
         .arg(
-            arg!(
-                -r "recursively downloads the images in all URLs received as parameters"
-            )
+            Arg::new("recursive").short('r').action(ArgAction::SetTrue)
+            .help("recursively downloads the images in all URLs received as parameters")
             .required(false)
         )
         .arg(
-            arg!(
-                -l <N> "indicates the maximum depth level of the recursive download. If not indicated, it will be 5"
-            )
+            Arg::new("length").short('l')
+            .help("indicates the maximum depth level of the recursive download. If not indicated, it will be 5")
             .required(false)
         )
         .arg(
-            arg!(
-                -p <PATH> "indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used."
-            )
+            Arg::new("to-path").short('p')
+            .help("indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used.")
             .required(false)
-        )
-        .arg(Arg::new("URLS").action(ArgAction::Append).num_args(1..))
+            )
+        .arg(Arg::new("URLS").action(ArgAction::Append).num_args(1..).required(true))
         .get_matches();
 
     
     let mut query = Query::new();
-    if matches.get_flag("r") {query.r_flag = true;}
-    if matches.get_flag("l") {
-        match matches.get_one("N") {
-            Some(n) => query.l_flag = *n,
-            None    => query.l_flag = 5,
-        };
-    }
-    if matches.get_flag("p") {
-        match matches.get_one("PATH") {
-            Some(path)  => query.p_flag = String::from(path as &str),
-            None        => query.p_flag = "./data/".to_string(),
-        }
-    }
+    if matches.get_flag("recursive") {query.r_flag = true;}
+    match matches.get_one::<String>("length") {
+        Some(n) => query.l_flag = (*n).parse::<usize>()?,
+        None    => query.l_flag = 5,
+    };
+    match matches.get_one::<String>("to-path") {
+        Some(path)  => query.p_flag = String::from(path as &str),
+        None        => query.p_flag = "./data/".to_string(),
+    };
     match matches.get_many::<String>("URLS") {
         Some(urls)  => query.urls = urls.map(|v| v.as_str().to_string()).collect::<Vec<_>>(),
-        None        => eprintln!("No URL provided"),
+        None        => panic!("No URL provided"),
     };
 
 
@@ -91,10 +104,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     eprintln!("{}", "Parsing responses...".green());
     spinner.enable_steady_tick(Duration::from_millis(100));
+    let mut images: Vec<String> = Vec::new();
     for response in responses {
         match response {
-            Ok(content) => println!("{} {}", "Fetched content:".green(), content),
+            Ok(content) => {
+                println!("{}", "Reading content...".green());
+                let document = Html::parse_document(content.as_str());
+                let selector = Selector::parse("img")?;
+                for node in document.select(&selector) {
+                    match node.attr("src") {
+                        Some(img)   => images.push(img.to_string()),
+                        None        => eprintln!("No images in node img"),
+                    }
+                }
+            },
             Err(e)      => eprintln!("{} {}", "Error:".red(), e),
+        };
+    }
+    if !query.r_flag {
+        download(String::from(images[0].as_str()), 0, query.p_flag, client.clone()).await?;
+    } else {
+        let mut i: usize = 0;
+        for img in images {
+            if i == query.l_flag {
+                break;
+            }
+            download(String::from(img.as_str()), i, String::from(query.p_flag.as_str()), client.clone()).await?;
+            println!("{} {}", "Downloaded:".blue(), img);
+            i += 1;
         }
     }
 
